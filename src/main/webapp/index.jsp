be62f628e64357631ad23132b7542238140b8341
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2014-12-21 23:34:52 -0200

    Adicionando AngularJS

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,10 +4,10 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script>
-        <script src="test.js"></script>
-        <title>JSP Page</title>
+        <script src="resources/script/test.js"></script>
+        <title>Test Service Test</title>
     </head>
-    <body ng-controler=""">
-        <h1>Hello World!</h1>
+    <body ng-controler="TestController">
+        <h1>Test Service Test</h1>
     </body>
 </html>
--- src/main/webapp/resources/script/test.js
+++ src/main/webapp/resources/script/test.js
@@ -1,20 +1,26 @@
 angular.module('testApp', []);
 
-defaultUrl="http://localhost:8080/crudpeople/testservice/";
+defaultUrl = "http://localhost:8080/crudpeople/testservice/";
 
 testApp.controller('TestController', ['$scope', '$http',
     function($scope, $http) {
         
         $scope.callTest = function() {
-            
-        };
+            $http.get(defaultUrl + 'test')
+                .succes(function(data, status) {
+                    $scope.testReturn = {'message' : data, 'status': status };
+                })
+                .error(function(data, status) {
+                    $scope.testReturn = {'message' : data, 'status': status };
+                });
+        }
         
         $scope.echo = function() {
             
-        };
+        }
         
         $scope.api = function() {
             
-        };
+        }
         
 }]);
