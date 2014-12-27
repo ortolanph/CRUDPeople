59abbd1ec559b400cf7f0074b0280fdc479936cb
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2014-12-27 15:46:34 -0200

    Modificando a classe de entidade

--- src/main/java/org/crudpeople/entities/Person.java
+++
@@ -1,8 +0,0 @@
-package org.crudpeople.entities;
-
-public class Person {
-    private Integer id;
-    private String name;
-    private String address;
-    private String phone;
-}
---
+++ src/main/java/org/crudpeople/entities/Pessoa.java
@@ -0,0 +1,8 @@
+package org.crudpeople.entities;
+
+public class Pessoa {
+    private Integer id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+}
